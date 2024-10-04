baffb6cbf82caa14caac4f989e34e2fb16191e06
Ryan Graham <ryangraham05824@outlook.com>
2024-10-03 20:09:50 -0500

    Started project 2

---
+++ Temperature/conversions.h
@@ -0,0 +1,10 @@
+#ifndef Conversions_H
+#define Conversions_H
+float celciusToFarenheit(float celcius);
+float celciusToKelvin(float celcius);
+float farenheitToCelcius(float farenheit);
+float farenheitToKelvin(float farenheit);
+float kelvinToCelcius(float kelvin);
+float kelvinToFarenheit(float kelvin);
+
+#endif
